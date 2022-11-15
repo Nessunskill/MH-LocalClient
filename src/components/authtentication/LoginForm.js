fb40ecdc1e9e07f6db6b4f2a172a55a231c8afe8
Nessunskill <fazly.iura@gmail.com>
2022-11-16 00:54:10 +0200

    yup schema

--- src/components/authtentication/LoginForm.js
+++ src/components/authtentication/LoginForm.js
@@ -1,17 +1,24 @@
-import withAuth from "../../hoc/withAuth";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { login } from "../../redux/slices/authSlice";
 import "./authenticationForm.scss";
 
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const LoginForm = () => {
     const dispatch = useDispatch();
 
+    const schema = yup.object({
+        username: yup.string().min(4, "Длина логина должна быть не менее 4 символов").max(12, "Длина логина должна быть не более 12 символов"),
+    })
+
     console.log('render')
 
-    const { register, handleSubmit} = useForm();
+    const { register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    });
 
     const onSubmit = data => {
         console.log(data)
@@ -24,6 +31,7 @@ const LoginForm = () => {
             <form onSubmit={handleSubmit(onSubmit)} className="auth__form form">
                 <label htmlFor="username" className="form__label">Username</label>
                 <input {...register("username")} type="text" name="username" placeholder="Введите логин..." className="form__field"/>
+                <p>{errors.username?.message}</p>
 
                 <label htmlFor="username" className="form__label">Password</label>
                 <input {...register("password")} type="password" name="password" placeholder="Введите пароль..." className="form__field"/>
